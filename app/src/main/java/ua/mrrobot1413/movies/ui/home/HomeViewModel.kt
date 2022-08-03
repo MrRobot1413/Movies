5f0267058e06f9363fd7c0588c071e6bc463e393
MrRobot1413 <koshlyak.31@gmail.com>
2022-08-02 21:15:59 -0400

    Small fixes

--- app/src/main/java/ua/mrrobot1413/movies/ui/home/HomeViewModel.kt
+++ app/src/main/java/ua/mrrobot1413/movies/ui/home/HomeViewModel.kt
@@ -7,17 +7,15 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.cancel
+import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.async
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.launch
 import ua.mrrobot1413.movies.App
 import ua.mrrobot1413.movies.data.network.model.Movie
-import ua.mrrobot1413.movies.data.network.model.MoviesResponse
 import ua.mrrobot1413.movies.data.network.model.Result
 import ua.mrrobot1413.movies.domain.useCase.GetPopularMoviesUseCase
 import ua.mrrobot1413.movies.domain.useCase.GetTopRatedMoviesUseCase
@@ -31,57 +29,72 @@ class HomeViewModel @Inject constructor(
     private val getUpcomingMoviesUseCase: GetUpcomingMoviesUseCase
 ) : ViewModel() {
 
-    private val _popularMovies = MutableLiveData<Result<MoviesResponse>?>()
-    val popularMovies: LiveData<Result<MoviesResponse>?> = _popularMovies
+    private val _popularMovies = MutableLiveData<Result<PagingData<Movie>?>?>()
+    val popularMovies: LiveData<Result<PagingData<Movie>?>?> = _popularMovies
 
-    private val _topRatedMovies = MutableLiveData<Result<MoviesResponse>?>(null)
-    var topRatedMovies: LiveData<Result<MoviesResponse>?> = _topRatedMovies
+    private val _topRatedMovies = MutableLiveData<Result<PagingData<Movie>?>?>(null)
+    val topRatedMovies: LiveData<Result<PagingData<Movie>?>?> = _topRatedMovies
 
-    private val _upcomingMovies = MutableLiveData<Result<MoviesResponse>?>(null)
-    val upcomingMovies: LiveData<Result<MoviesResponse>?> = _upcomingMovies
+    private val _upcomingMovies = MutableLiveData<Result<PagingData<Movie>?>?>(null)
+    val upcomingMovies: LiveData<Result<PagingData<Movie>?>?> = _upcomingMovies
 
-    var popularPages = 1
-    var topPages = 1
-    var upcomingPages = 1
+    private var popularMoviesDeferred: Deferred<Flow<PagingData<Movie>?>?>? = null
+    private var topRatedMoviesDeferred: Deferred<Flow<PagingData<Movie>?>?>? = null
 
-    fun getPopularMovies(page: Int) {
-        viewModelScope.launch {
+    fun getMovies() {
+        getPopularMovies()
+        getTopRatedMovies()
+        getUpcomingMovies()
+    }
+
+    private fun getPopularMovies() {
+        popularMoviesDeferred = viewModelScope.async {
             _popularMovies.value = Result.loading(null)
-            var data: MoviesResponse? = null
-            try {
-                data = getPopularMoviesUseCase.invoke(page)
-                _popularMovies.value = Result.success(data)
+            return@async try {
+                getPopularMoviesUseCase.invoke().cachedIn(viewModelScope)
             } catch (e: Exception) {
-                println("Ex 1: ${e.message}")
-                _popularMovies.value = Result.error(data, e.message)
+                println("Ex: ${e.message}")
+                _popularMovies.value = Result.error(null, e.message)
+                null
             }
         }
     }
 
-    fun getTopRatedMovies(page: Int) {
-        viewModelScope.launch {
+    private fun getTopRatedMovies() {
+        topRatedMoviesDeferred = viewModelScope.async {
             _topRatedMovies.value = Result.loading(null)
-            var data: MoviesResponse? = null
-            return@launch try {
-                data = getTopRatedMoviesUseCase.invoke(page)
-                _topRatedMovies.value = Result.success(data)
+            return@async try {
+                getTopRatedMoviesUseCase.invoke().cachedIn(viewModelScope)
             } catch (e: Exception) {
-                println("Ex 2: ${e.message}")
-                _topRatedMovies.value = Result.error(data, e.message)
+                println("Ex: ${e.message}")
+                _topRatedMovies.value = Result.error(null, e.message)
+                null
             }
         }
     }
 
-    fun getUpcomingMovies(page: Int) {
+    private fun getUpcomingMovies() {
         viewModelScope.launch {
             _upcomingMovies.value = Result.loading(null)
-            var data: MoviesResponse? = null
             try {
-                data = getUpcomingMoviesUseCase.invoke(page)
-                _upcomingMovies.value = Result.success(data)
+                getUpcomingMoviesUseCase.invoke().cachedIn(viewModelScope).collect { upcoming ->
+                    // Awaiting popular movies
+                    popularMoviesDeferred?.await()?.collect { popular ->
+                        // Awaiting top movies
+                        topRatedMoviesDeferred?.await()?.collect { topRated ->
+                            // Emit popular movies
+                            delay(1000)
+                            _popularMovies.value = Result.success(popular)
+                            // Emit top movies
+                            _topRatedMovies.value = Result.success(topRated)
+                            // Emit upcoming movies
+                            _upcomingMovies.value = Result.success(upcoming)
+                        }
+                    }
+                }
             } catch (e: Exception) {
-                println("Ex 3: ${e.message}")
-                _topRatedMovies.value = Result.error(data, e.message)
+                println("Ex: ${e.message}")
+                _topRatedMovies.value = Result.error(null, e.message)
             }
         }
     }
