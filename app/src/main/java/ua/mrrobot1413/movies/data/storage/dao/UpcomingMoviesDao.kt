716f1306f7ff590a3930a36800c6a44c3e09495b
MrRobot1413 <koshlyak.31@gmail.com>
2022-08-14 18:21:26 -0400

    Offline caching provided

--- app/src/main/java/ua/mrrobot1413/movies/data/storage/dao/UpcomingMoviesDao.kt
+++ app/src/main/java/ua/mrrobot1413/movies/data/storage/dao/UpcomingMoviesDao.kt
@@ -1,6 +1,5 @@
 package ua.mrrobot1413.movies.data.storage.dao
 
-import androidx.paging.PagingSource
 import androidx.room.*
 import kotlinx.coroutines.flow.Flow
 import ua.mrrobot1413.movies.data.storage.model.UpcomingMovie
